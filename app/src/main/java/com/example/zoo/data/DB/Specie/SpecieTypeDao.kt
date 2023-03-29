f5d657fcfa992582b4995d3db45af71a16685ee3
Alexander Garcia <alexandergv2117@gmail.com>
2023-03-28 20:08:38 -0500

    Se resolvio el error de compilacion

--- app/src/main/java/com/example/zoo/data/DB/Specie/SpecieTypeDao.kt
+++ app/src/main/java/com/example/zoo/data/DB/Specie/SpecieTypeDao.kt
@@ -1,10 +1,12 @@
 package com.example.zoo.data.DB.Specie
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.zoo.data.DB.Specie.SpecieTypeEntity
 
+@Dao
 interface SpecieTypeDao {
     @Query("SELECT * FROM specie")
     suspend fun getAllSpecie(): List<SpecieTypeEntity>
