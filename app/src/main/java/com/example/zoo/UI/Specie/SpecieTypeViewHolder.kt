b347cf7f9a8f0d351cdb7d884483dbd5949b481d
Alexander Garcia <alexandergv2117@gmail.com>
2023-03-29 13:41:02 -0500

    Ya se funciona la conexión a la API y la base de datos local

--- app/src/main/java/com/example/zoo/UI/Specie/SpecieTypeViewHolder.kt
+++ app/src/main/java/com/example/zoo/UI/Specie/SpecieTypeViewHolder.kt
@@ -2,9 +2,10 @@ package com.example.zoo.UI.Specie
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.zoo.data.model.SpecieTypeModel
 import com.example.zoo.databinding.ItemSpeciesBinding
+import com.example.zoo.utils.Const
+import com.squareup.picasso.Picasso
 
 class SpecieTypeViewHolder(
     private val itemView: View
@@ -14,6 +15,8 @@ class SpecieTypeViewHolder(
     fun bind(item: SpecieTypeModel) {
         binding.specieId.text = item.id.toString()
         binding.specieName.text = item.tipo
-        Glide.with(itemView.context).load(item.link_foto).into(binding.specieImage)
+        Picasso.get()
+            .load("${Const.BASE_URL_GOOGLE_DRIVE}${item.link_foto}")
+            .into(binding.specieImage)
     }
 }
--- app/src/main/java/com/example/zoo/utils/Const.kt
+++ app/src/main/java/com/example/zoo/utils/Const.kt
@@ -2,6 +2,8 @@ package com.example.zoo.utils
 
 object Const {
     const val DB_NAME = "zoo"
-    const val API_BASE_URL = "https://aefe-187-252-194-80.ngrok.io/"
+    const val API_BASE_URL = "https://bd14-187-252-194-80.ngrok.io/"
     const val TIMEOUT: Long = 5000
+
+    const val BASE_URL_GOOGLE_DRIVE = "https://drive.google.com/uc?export=view&id="
 }
