51aec50b6bc97a944a45acc41c08ada2e00f763d
Alexander Garcia <alexandergv2117@gmail.com>
2023-03-31 19:01:14 -0500

    ya funciona el logIn

--- app/src/main/java/com/example/zoo/utils/Const.kt
+++ app/src/main/java/com/example/zoo/utils/Const.kt
@@ -2,14 +2,10 @@ package com.example.zoo.utils
 
 object Const {
     const val DB_NAME = "zoo"
-    const val API_BASE_URL = "https://ae70-187-252-201-186.ngrok.io/"
+    const val API_BASE_URL = "https://6ce4-187-252-201-186.ngrok.io/"
     const val TIMEOUT: Long = 5000
 
     const val BASE_URL_GOOGLE_DRIVE = "https://drive.google.com/uc?export=view&id="
-
-    const val RC_SIGN_IN = 9001
-    const val TAG = "MyFragment"
-    const val default_web_client_id = "663801718359-dl51r3ouo43fluncq9k9a68cbk5je26d.apps.googleusercontent.com"
 
     var isLoggedIn: Boolean = false
 }
