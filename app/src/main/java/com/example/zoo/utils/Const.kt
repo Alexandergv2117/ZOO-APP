0f68257373f127099b43c2ee421fd94ed8b62e57
Alexander Garcia <74172014+Alexandergv2117@users.noreply.github.com>
2023-04-05 13:42:18 -0500

    Ya se pasa el tipo de especie por safeArgs

--- app/src/main/java/com/example/zoo/utils/Const.kt
+++ app/src/main/java/com/example/zoo/utils/Const.kt
@@ -2,7 +2,7 @@ package com.example.zoo.utils
 
 object Const {
     const val DB_NAME = "zoo"
-    const val API_BASE_URL = "https://6ce4-187-252-201-186.ngrok.io/"
+    const val API_BASE_URL = "https://320a-187-252-196-216.ngrok.io/"
     const val TIMEOUT: Long = 5000
 
     const val BASE_URL_GOOGLE_DRIVE = "https://drive.google.com/uc?export=view&id="
