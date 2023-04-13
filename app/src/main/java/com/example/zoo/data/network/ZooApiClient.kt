27a367a7bf5ae48d8e45e3286d6b23948087b3e6
Isaac Master <94545671+IsaacQuirozMasters@users.noreply.github.com>
2023-04-12 20:41:13 -0500

    Comentario-1 (#9)
    * Comentario-1
    
    Actualización 1 de Comentarios
    
    * Comentario-2
    
    -Comentario
    --Comentario POST
    --Comentario Layout
    --Comentario Alert (Falso -- pls Implentar)
    -Calificacion
    --Layout Bar
    -- Theme/Style Bar
    --POST calificacion
    --Layout Calificacion
    --Alert (True)
    
    ---------
    
    Co-authored-by: Alexander Garcia <74172014+Alexandergv2117@users.noreply.github.com>

--- app/src/main/java/com/example/zoo/data/network/ZooApiClient.kt
+++ app/src/main/java/com/example/zoo/data/network/ZooApiClient.kt
@@ -5,11 +5,23 @@ import com.example.zoo.data.model.SpecieTypeModel
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.Call
 
 interface ZooApiClient {
     @GET("especie")
     suspend fun getAllSpecie(): Response<List<SpecieTypeModel>>
-
+    
     @GET("especie/tipo")
     suspend fun getAnimalBySpecie(@Query("tipo") tipo: String): Response<List<AnimalTypeModel>>
+
+    @POST ("comentario")
+    fun enviarComentario(@Body enviarComentarioDto:EnviaComentarioDto):Call<RespuestaComentario>
+    @POST ("calificacion")
+    fun enviarCalificacion(@Body enviarCalificacionDto:EnviaCalificacionDto):Call<RespuestaCalificacion>
 }
+data class EnviaComentarioDto(val content: String)
+data class RespuestaComentario(val respuesta: String)
+data class EnviaCalificacionDto(val calificacion: Float)
+data class RespuestaCalificacion(val respuesta: String)
